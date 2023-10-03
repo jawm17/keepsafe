f6043c89cdfa934f3b2345d10103539a314821d1
Envy Hose <envyhose@Envys-MBP.lan>
2023-10-02 22:49:07 -0500

    stuff

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import HeroVid from "../../assets/trafic.mp4";
 import LogoPng from "../../assets/logo.png";
+import Form from "../../components/Form";
 import "./homeStyle.css";
 
 export default function Home() {
     const [menuOpen, setMenuOpen] = useState(false);
     const [scrolled, setScrolled] = useState(false);
+    const [highlight, setHighlight] = useState(0);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -25,8 +27,50 @@ export default function Home() {
 
     return (
         <div>
-            <div id="header">
-
+            <div id={scrolled || menuOpen ? "headerScrolled" : "headerInitial"}>
+                <img id="headerLogo" src={LogoPng}></img>
+                <div id="headerBtnArea">
+                    <div className="headerBtn" onClick={() => window.location.reload()}>
+                        home
+                    </div>
+                    <div className="headerBtn" onClick={() => scroll("actionSection")}>
+                        solutions
+                    </div>
+                    <div className="headerBtn" onClick={() => scroll("supportersSection")}>
+                        supporters
+                    </div>
+                    <div className="headerBtn" onClick={() => scroll("formSection")}>
+                        contact
+                    </div>
+                </div>
+                <div id="headerActNowBtn">
+                    act now
+                </div>
+                <svg onClick={() => setMenuOpen(!menuOpen)} id="menuBtn" style={menuOpen ? { display: "none" } : null} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+                <svg onClick={() => setMenuOpen(!menuOpen)} id="menuBtn" style={!menuOpen ? { display: "none" } : null} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </div>
+            <div id={menuOpen ? "dropDownMenuOpen" : "dropDownMenu"} className="dropDownMenu">
+                <div id="menuOptionArea">
+                    <div className="menuOption" onClick={() => window.location.reload()}>
+                        home
+                    </div>
+                    <div className="menuOption" onClick={() => scroll("actionSection")}>
+                        solutions
+                    </div>
+                    <div className="menuOption" onClick={() => scroll("supportersSection")}>
+                        supporters
+                    </div>
+                    <div className="menuOption" onClick={() => scroll("formSection")}>
+                        contact
+                    </div>
+                    <div id="actNowMenuBtn" onClick={() => scroll("formSection")}>
+                        act now!
+                    </div>
+                </div>
             </div>
 
             <div id="heroSection">
@@ -57,13 +101,111 @@ export default function Home() {
 
                     </div>
                 </div>
-                <video id="heroVid" autoPlay muted loop>
+                {/* <video id="heroVid" autoPlay muted loop>
                     <source src={HeroVid} type="video/mp4" />
-                </video>
+                </video> */}
+                <img id="heroMedia" src="https://testsiterd.kinsta.cloud/wp-content/uploads/2023/04/Background.png"></img>
             </div>
 
             <div id="section1">
+                <div id="section1Flex">
+                    <div id="section1Area">
+                        <div id="section1TopText">
+                            Our Mission
+                        </div>
+                        <div id="section1HeaderText">
+                            We are a coalition of local businesses advocating for new sign rules
+                        </div>
+                        <div id="section1SubText">
+                            Empower Round Rock with Electronic Message Center (EMC) signs: a pivotal tool for economic growth, heightened public safety, and emergency responsiveness. Restricted to I-35 and SH-45 only, EMC signs respect our city’s serene and scenic areas and will be limited to high-traffic commercial corridors only. Let’s modernize Round Rock’s sign regulations and champion a safer, prosperous future for us all.
+                        </div>
+                    </div>
+                </div>
+                <div id="section1ImgFlex">
+                    <div id="section1ImgArea">
+                        <div className="section1Highlight" id={highlight === 1 ? "highlightOpen" : ""} onMouseEnter={() => setHighlight(1)} onMouseLeave={() => setHighlight(0)}>
+                            <div className="highlightBlur">
+                                <div className="highlightFade">
+                                    <div className="highlightTitle">
+                                        enhancing safety
+                                    </div>
+                                </div>
+                                <div className="learnMoreHighlight">
+                                    <div className="highlightInfo">
+                                        Let’s modernize Round Rock’s sign regulations and champion a safer, prosperous future for us all.
+                                    </div>
+                                </div>
+                                <div className="learnMoreBtn">
+                                    <div>
+                                        show {highlight === 1 ? "less" : "more"}
+                                    </div>
+                                    <svg id="learnMoreIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="section1Highlight" id={highlight === 2 ? "highlightOpen" : ""} onMouseEnter={() => setHighlight(2)} onMouseLeave={() => setHighlight(0)}>
+                            <div className="highlightBlur">
+                                <div className="highlightFade">
+                                    <div className="highlightTitle">
+                                        promoting growth
+                                    </div>
+                                </div>
+                                <div className="learnMoreHighlight">
+                                    <div className="highlightInfo">
+                                        Let’s modernize Round Rock’s sign regulations and champion a safer, prosperous future for us all.
+                                    </div>
+                                </div>
+                                <div className="learnMoreBtn">
+                                    <div>
+                                        show {highlight === 2 ? "less" : "more"}
+                                    </div>
+                                    <svg id="learnMoreIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="section1Highlight" id={highlight === 3 ? "highlightOpen" : ""} onMouseEnter={() => setHighlight(3)} onMouseLeave={() => setHighlight(0)}>
+                            <div className="highlightBlur">
+                                <div className="highlightFade">
+                                    <div className="highlightTitle">
+                                        supporting community
+                                    </div>
+                                </div>
+                                <div className="learnMoreHighlight">
+                                    <div className="highlightInfo">
+                                        Let’s modernize Round Rock’s sign regulations and champion a safer, prosperous future for us all.
+                                    </div>
+                                </div>
+                                <div className="learnMoreBtn">
+                                    <div>
+                                        show {highlight === 3 ? "less" : "more"}
+                                    </div>
+                                    <svg id="learnMoreIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <img className="section1Img" src="https://testsiterd.kinsta.cloud/wp-content/uploads/2023/04/round-rock-image.jpg"></img>
+                        <img className="section1Img" src="https://testsiterd.kinsta.cloud/wp-content/uploads/2023/04/round-rock-image.jpg"></img>
+                        <img className="section1Img" src="https://testsiterd.kinsta.cloud/wp-content/uploads/2023/04/round-rock-image.jpg"></img> */}
+                    </div>
+                </div>
+            </div>
+            <div id="section2">
+                <div id="section2Area">
+                    <div id="supportersLeft">
+                        <div id="supportersTitle">
+                            Our Supporters
+                        </div>
+                    </div>
+                    <div id="supportersRight">
 
+                    </div>
+                </div>
             </div>
         </div>
     );
