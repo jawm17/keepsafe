5326fe6ce7ba4022f17a7c43df4137c22f08f185
jonesliddy77 <jonesliddy80@yahoo.com>
2023-10-06 15:31:04 -0500

    fonts and shit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-import logo from "../assets/keepsafeLogo.jpeg";
+import LogoPng from "../assets/logo.png";
 import "./styles/headerStyle.css";
 
 export default function Header() {
-    const location = "Round Rock";
     const [menuOpen, setMenuOpen] = useState(false);
     const [scrolled, setScrolled] = useState(false);
 
@@ -17,52 +16,48 @@ export default function Header() {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
+
     function scroll(section) {
         document.getElementById(section).scrollIntoView({ behavior: 'smooth' });
         setMenuOpen(false);
     }
-
     return (
-        <div>
-            <div className={`headerNavbar ${scrolled || menuOpen ? 'scrolled' : ''}`}>
-                <div id="headerInner">
-                    <div id="logoHeader" onClick={() => window.location.reload()}>
-                    {/* <img src="https://testsiterd.kinsta.cloud/wp-content/uploads/2023/04/footer-experiment-Keep-Round-Rock-Safe-04.png"></img> */}
-                        </div>
-                    <div id="headerBtnArea">
-                        <div className="headerBtn" onClick={() => window.location.reload()}>
-                            home
-                        </div>
-                        <div className="headerBtn" onClick={() => scroll("actionSection")}>
-                            solutions
-                        </div>
-                        <div className="headerBtn" onClick={() => scroll("supportersSection")}>
-                            supporters
-                        </div>
-                        <div className="headerBtn" onClick={() => scroll("formSection")}>
-                            contact
-                        </div>
+        <>
+            <div id={scrolled || menuOpen ? "headerScrolled" : "headerInitial"}>
+                <img id="headerLogo" src={LogoPng} onClick={() => scroll("heroSection")}></img>
+                <div id="headerBtnArea">
+                    <div className="headerBtn" onClick={() => scroll("heroSection")}>
+                        home
                     </div>
-                    <div id="actNowBtn" onClick={() => scroll("formSection")}>
-                        act now
+                    <div className="headerBtn" onClick={() => scroll("section1")}>
+                        about
                     </div>
-                    <svg onClick={() => setMenuOpen(!menuOpen)} id="menuBtn" style={menuOpen ? { display: "none" } : null} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                    <svg onClick={() => setMenuOpen(!menuOpen)} id="menuBtn" style={!menuOpen ? { display: "none" } : null} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
+                    <div className="headerBtn" onClick={() => scroll("section2")}>
+                        supporters
+                    </div>
+                    <div className="headerBtn" onClick={() => scroll("formSection")}>
+                        contact
+                    </div>
                 </div>
+                <div id="headerActNowBtn" onClick={() => scroll("formSection")}>
+                    act now
+                </div>
+                <svg onClick={() => setMenuOpen(!menuOpen)} id="menuBtn" style={menuOpen ? { display: "none" } : null} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+                <svg onClick={() => setMenuOpen(!menuOpen)} id="menuBtn" style={!menuOpen ? { display: "none" } : null} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
             </div>
             <div id={menuOpen ? "dropDownMenuOpen" : "dropDownMenu"} className="dropDownMenu">
                 <div id="menuOptionArea">
-                    <div className="menuOption" onClick={() => window.location.reload()}>
+                    <div className="menuOption" onClick={() => scroll("heroSection")}>
                         home
                     </div>
-                    <div className="menuOption" onClick={() => scroll("actionSection")}>
-                        solutions
+                    <div className="menuOption" onClick={() => scroll("section1")}>
+                        about
                     </div>
-                    <div className="menuOption" onClick={() => scroll("supportersSection")}>
+                    <div className="menuOption" onClick={() => scroll("section2")}>
                         supporters
                     </div>
                     <div className="menuOption" onClick={() => scroll("formSection")}>
@@ -73,7 +68,7 @@ export default function Header() {
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 
 }
