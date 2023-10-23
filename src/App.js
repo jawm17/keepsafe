972d1497a034515916d9b9bc2e818fed3641db3c
jonesliddy77 <jonesliddy80@yahoo.com>
2023-10-23 11:46:26 -0500

    link

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={NoMatchPage} />
+        <Route path="/" component={Home} />
         {/* <Route path="/" component={NoMatchPage} /> */}
       </Switch>
     </BrowserRouter>
