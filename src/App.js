1f916f856d4cd78f471ce66a4e78b8cb79d593ca
jonesliddy77 <jonesliddy80@yahoo.com>
2023-10-24 03:06:42 -0500

    idk

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import { Component } from "react";
 import "./index.css";
+import ReactGA from 'react-ga';
 
 // Pages
 import Home from "./pages/Home/Home";
 
-function App() {
-  return (
+class App extends Component {
+  setGA = () => {
+    ReactGA.initialize('UA-290625727-1');
+    ReactGA.pageview('Init page view');
+  };
+  componentDidMount(){
+    this.setGA();
+  }
+  render() {
+    return (
     <Home /> 
-  );
+    );
+  }
 }
 
 export default App;
