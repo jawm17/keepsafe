c5b80756f3a92bf2ee746805cfda881728c002bd
jonesliddy77 <jonesliddy80@yahoo.com>
2023-10-24 03:38:00 -0500

    ga add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import { Component } from "react";
+import React from 'react';
 import "./index.css";
-import ReactGA from 'react-ga';
 
 // Pages
 import Home from "./pages/Home/Home";
 
-class App extends Component {
-  setGA = () => {
-    ReactGA.initialize('UA-290625727-1');
-    ReactGA.pageview('Init page view');
-  };
-  componentDidMount(){
-    this.setGA();
-  }
-  render() {
-    return (
+function App() {
+  return (
     <Home /> 
-    );
-  }
+  );
 }
 
 export default App;
