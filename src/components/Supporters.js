3ea115298a0e3f7c59c4cf01e812060cd91bb838
jonesliddy77 <jonesliddy80@yahoo.com>
2023-11-14 13:45:04 -0600

    gettting rid of mod and cendo

--- src/components/Supporters.js
+++ src/components/Supporters.js
@@ -20,31 +20,31 @@ const Supporters = () => {
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Buffet.png?alt=media&token=58d53753-5b2c-4f32-97e2-88bc25867d86" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Razzoos.png?alt=media&token=239be2af-0b0b-4775-81f5-783e3f1511bf" />
             <img loading="lazy"  src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/SMOKEHOUSE.png?alt=media&token=af061eef-14cd-4cdc-9b36-aa037eeb6ae9" />
-            {/* <img loading="lazy" className="whiteLogo" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/UrbanCreetside.png?alt=media&token=db8a7526-b5d6-424e-9a3e-a412880b4203" /> */}
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Screenshot%20.png?alt=media&token=d193b537-7644-4cf1-a61c-2138779cfd5d" />
+            {/* <img loading="lazy" className="whiteLogo" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/UrbanCreetside.png?alt=media&token=db8a7526-b5d6-424e-9a3e-a412880b4203" /> */}
           </div>
           <div aria-hidden="true" className="marquee__group">
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Cajun.png?alt=media&token=69bd4bfc-d499-4e75-af52-9b0f1c148392" />
-            <img loading="lazy" className="whiteLogo" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Centro.png?alt=media&token=f87d239c-36fc-466b-9f3a-32dd2fa403d3" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/CleanEatz.png?alt=media&token=5381db1a-60be-4bcb-964f-deaa7762ff76" />
             <img loading="lazy" className="whiteLogo" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Finleys.png?alt=media&token=6770daf5-3638-478e-a671-2936b2c3ac06" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Ginos.png?alt=media&token=eea17018-37a1-4ebd-a52d-d1abb850499b" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/RioGrande.png?alt=media&token=1a1e7d00-2194-47e1-a0dc-fdabbcda9646" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/SnackRoom.png?alt=media&token=c6479257-c987-4d45-9a4d-184c8367694e" />
-            {/* <img loading="lazy" className="whiteLogo" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/UrbanRoof.png?alt=media&token=9d12b464-9a32-43d8-aaf3-a41726121a46" /> */}
+            <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/HURRICANE.png?alt=media&token=a7270f56-8d9c-4ee1-b2c0-6920feb6a338" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/NEW_Playtri.png?alt=media&token=851cbb28-1f91-48e3-83d9-bbefcb25d751" />
+            {/* <img loading="lazy" className="whiteLogo" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/UrbanRoof.png?alt=media&token=9d12b464-9a32-43d8-aaf3-a41726121a46" /> */}
 
           </div>
         </div>
         <div className="marquee marquee--reverse">
           <div className="marquee__group">
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/HAIRSALON.png?alt=media&token=6fa88619-b3dd-4a7c-88ea-4c603ef1032a" />
-            <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/HURRICANE.png?alt=media&token=a7270f56-8d9c-4ee1-b2c0-6920feb6a338" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/PrincessCraftRV.jpeg?alt=media&token=d6036127-906b-4cd8-afb7-b8b301bfdd41" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/KenneyFort.png?alt=media&token=2c0212c0-956a-4bed-aeee-d01d25bb24ea" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/LaConcha.png?alt=media&token=6301fe81-7de0-4fe2-855e-44be4361d3c3" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/ROUND%20RISK-01.jpg?alt=media&token=c499b8b5-7910-461b-bd3d-56532c0f5d05" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Tequila.png?alt=media&token=17e15b99-16ee-4c2b-92ce-c0442d52405c" />
+            {/* <img loading="lazy" className="whiteLogo" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Centro.png?alt=media&token=f87d239c-36fc-466b-9f3a-32dd2fa403d3" /> */}
             <img loading="lazy" src={Idk} />
 
           </div>
@@ -52,11 +52,11 @@ const Supporters = () => {
             <img loading="lazy"  src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/LAMPPOST-01.png?alt=media&token=7a86ffcc-0554-4ff6-a1b5-314f74d958dc" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/LibertyBarbeque.png?alt=media&token=adaa0a5e-5226-48e5-854c-a4e3543148ea" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/MicroBac.png?alt=media&token=9b4ba8b0-93be-4405-9728-0973ce492ee0" />
-            <img loading="lazy" className="whiteLogo" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/MOD.png?alt=media&token=954d87ac-780d-4991-acf3-012d9e8413f9" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Palermo-01.jpg?alt=media&token=241d33f3-6374-4b94-bc46-e8916edc2dd0" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Santiagos.png?alt=media&token=301e37fd-e078-4b09-8cef-0142881573cb" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Tokyo.png?alt=media&token=2940ecbb-d107-4cf9-94f6-3a92b875a1bd" />
             <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/WARPATHPIZZA.png?alt=media&token=b96162c5-e0f0-439b-baa3-8c3b4fbfe405" />
+            {/* <img loading="lazy" className="whiteLogo" src="https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/MOD.png?alt=media&token=954d87ac-780d-4991-acf3-012d9e8413f9" /> */}
           </div>
         </div>
       </article>
