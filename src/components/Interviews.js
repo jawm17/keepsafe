979741cb44a6e0475f3ede16c5a2dfb5df97f7a9
jonesliddy77 <jonesliddy80@yahoo.com>
2023-12-15 05:06:51 -0600

    new vid

--- src/components/Interviews.js
+++ src/components/Interviews.js
@@ -5,7 +5,16 @@ const createVideoProps = () => ({
   'data-video-id': 'ABC123',
   "src":"https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Robert%20Tamble%20Testimonial.mp4?alt=media&token=9ca5db27-5c48-4f4a-8a65-b6e01f271b99" ,
   'controls': true,
-  'loop': true,
+  // 'loop': true,
+  'muted': true,
+  'data-yes': true,
+  'data-no': false,
+});
+const createVideoProps2 = () => ({
+  'data-video-id': 'ABC123',
+  "src":"https://firebasestorage.googleapis.com/v0/b/roundrock-fcf1c.appspot.com/o/Media%20Choice%20-%20Gino's%20V5.mp4?alt=media&token=4df00c36-9362-4dd9-98c7-6430743db48a" ,
+  'controls': true,
+  // 'loop': true,
   'muted': true,
   'data-yes': true,
   'data-no': false,
@@ -13,15 +22,18 @@ const createVideoProps = () => ({
 
 const Interviews = () => {
   const [videoProps, setVideoProps] = useState({});
+  const [videoProps2, setVideoProps2] = useState({});
 
   useEffect(() => {
     setVideoProps(createVideoProps());
+    setVideoProps2(createVideoProps2());
   }, []);
 
   return (
     <>
-    <h1>Testimonial</h1>
-      <video autoPlay="true" playsInline {...videoProps}/>
+    <h1>Testimonials</h1>
+      <video autoPlay="false" playsInline {...videoProps}/>
+      <video autoPlay="false" playsInline {...videoProps2}/>
     </>
   );
 };
