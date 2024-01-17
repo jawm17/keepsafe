27e01a6256b22a3a3371ecae06d18aaa87af0330
Envy Hose <envyhose@Envys-MBP.attlocal.net>
2024-01-17 00:02:30 -0600

    new page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import ScrollToTop from "./scrollToTop";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import "./index.css";
 
 // Pages
 import Home from "./pages/Home/Home";
+import News from './pages/News/News';
 
 function App() {
   return (
-    <Home /> 
+    <div>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/news" component={News} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
