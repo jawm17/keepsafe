f7153bbfbc64e0bc0747da570a039d463701474b
jonesliddy77 <jonesliddy80@yahoo.com>
2024-03-19 10:11:47 -0500

    adding new vid

--- src/components/Interviews.js
+++ src/components/Interviews.js
@@ -1,31 +1,38 @@
-import React, { useState, useEffect} from 'react';
-import "./styles/interviewsStyles.css"
-
+import React, { useState, useEffect } from "react";
+import "./styles/interviewsStyles.css";
 
 const Interviews = () => {
-  const [youtubeID, setYoutubeID] = useState('gD48Ose0sBw');
-
+  const [youtubeID, setYoutubeID] = useState("gD48Ose0sBw");
 
   return (
     <>
-    <h1>Testimonials</h1>
+      <h1>Testimonials</h1>
       <div id="video">
-      <iframe
-  className='video'
-  title='Youtube player'
-  sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
-  src={`https://youtube.com/embed/KVOkpFEgAhk?autoplay=0`}
-></iframe>
+        <iframe
+          className="video"
+          title="Youtube player"
+          sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
+          src={`https://youtube.com/embed/KVOkpFEgAhk?autoplay=0`}
+        ></iframe>
       </div>
       <div id="video">
-      <iframe
-  className='video'
-  title='Youtube player'
-  sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
-  src={`https://youtube.com/embed/${youtubeID}?autoplay=0`}
-></iframe>
+        <iframe
+          className="video"
+          title="Youtube player"
+          sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
+          src={`https://youtube.com/embed/${youtubeID}?autoplay=0`}
+        ></iframe>
       </div>
-      
+      <div id="video">
+        <iframe
+          className="video"
+          src="https://www.youtube.com/embed/_zSDg_M2M9o?si=4IVAAfi8wmtLKXBA"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+
       {/* <video autoPlay="false" playsInline {...videoProps2}/> */}
     </>
   );
